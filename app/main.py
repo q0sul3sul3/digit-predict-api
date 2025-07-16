05a3dee42587e96583137a4cc717728399fa9895
Grace <q0sul3sul3@gmail.com>
2025-07-16 18:35:06 +0800

    Add predict_batch API

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import FastAPI, File, UploadFile
 from PIL import Image
 
@@ -6,8 +8,18 @@ from .model import predict_digit
 app = FastAPI()
 
 
-@app.post("/predict")
+@app.post('/predict')
 async def predict(file: UploadFile = File(...)):
     image = Image.open(file.file)
     pred = predict_digit(image)
-    return {"prediction": pred}
+    return {'prediction': pred}
+
+
+@app.post('/predict_batch')
+async def predict_batch(files: List[UploadFile] = File(...)):
+    results = []
+    for file in files:
+        image = Image.open(file.file)
+        pred = predict_digit(image)
+        results.append({'filename': file.filename, 'prediction': pred})
+    return results
